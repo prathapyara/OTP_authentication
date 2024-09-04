5a5f8208e08d9b598f80c7aec42a18c11f7691aa
yara_ prathap_22768238 <prathapyara@gmaail.com>
2024-09-04 08:40:18 +0530

    mongoaddation

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -33,9 +33,9 @@ export default function Username(){
             <div className="flex justify-center items-center h-screen">
              <div className={styles.glass}>
                <div className="title flex flex-col items-center">
-                 <h4 className="text-5xl font-bold">Hello Again!</h4>
+                 <h4 className="text-5xl font-bold">Hello User!</h4>
                  <span className="py-4 text-xl w-2/3 text-center text-gray-500" >
-                    This is the test
+                    Have a great day..!
                  </span>
                </div>
 
